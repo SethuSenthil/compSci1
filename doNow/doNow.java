ee3e82c2ebcf254ba2f2ca7f5468927c804c3b6c
Sethu Narayan Senthil <sethunarayansenthil@gmail.com>
2018-10-01 10:49:04 -0400

    d2

---
+++ doNow/doNow.java
@@ -0,0 +1,13 @@
+public class doNow
+{
+	public static void printer(String str) {
+  		System.out.print(str);
+    }
+    public static void printerNew(String str) {
+			System.out.println(' ');
+	  		System.out.print(str);
+    }
+	public static void main (String[] args)
+	{
+       printer("sethu \tsenthil \nBlue\n9\\8\\10\n\"If you fail your first attempt call in version 1.0\"" );}
+}
