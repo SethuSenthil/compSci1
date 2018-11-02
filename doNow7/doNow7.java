8fd212a2051865855b98225a8811b0caaa80ae1d
Sethu Narayan Senthil <10023088@sbschools.org>
2018-11-02 11:37:24 -0400

    Do Now 7 and 8

---
+++ doNow7/doNow7.java
@@ -0,0 +1,29 @@
+public class doNow7 {
+
+	public static void main(String[] args) {
+		System.out.println(     quot(1,3))
+     avg(1,2,3,4,5);
+     intTo4(3);
+     dblTo4(3.2);
+	}
+
+	public static double quot(double num1 , double num2) {
+      double solved = num1/num2;
+      return solved;
+	}
+
+	public static double avg(int num1 , int num2, int num3, int num4, int num5 ) {
+      double solved = (num1 + num2 + num3 + num4 + num5)/5;
+      return solved;
+	}
+
+		public static int intTo4(int num){
+          int solved = num * num * num;
+          return solved;
+	}
+			public static double dblTo4(double num){
+	          double solved = num * num * num;
+	          return solved;
+	}
+
+}
---
+++ doNow8/doNow8.java
@@ -0,0 +1,17 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class doNow8 {
+
+public static void main(String[] args){
+out.println("no nut onv");
+out.println(yoo(8));
+
+}
+
+public static int  yoo (int num){
+
+return num;
+
+}
+}
