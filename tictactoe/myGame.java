16f25e626e539c2edaad3d2f4002a45d9e8306d1
Sethu Narayan Senthil <10023088@sbschools.org>
2019-03-15 12:10:39 -0400

    Update myGame.java

--- tictactoe/myGame.java
+++ tictactoe/myGame.java
@@ -7,13 +7,14 @@ public class myGame{
 				  						{'c','c','c'},
 				  						{'c','c','c'}
 					};
-			      public static char block1 = arr[0][0],block2  = arr[0][1],block3  = arr[0][2],block4 = arr[1][0],block5  = arr[1][1],block6  = arr[1][2],block7  = arr[2][0],block8  = arr[2][1],block9  =  arr[2][2];
 
 public static char xo = 'x';
 
 
 	public static void main(String[] args){
 //init game
+
+
  int[][] grid = {
 						{0,200,600,200},
 						{200,0,200,600},
@@ -24,69 +25,115 @@ boolean gameEnd = false;
 int lastPlay = 1;
 board.defineBoard(grid);
 board.setBoard(arr);
-board.setFiles("o.jpg","x.jpg" );
+board.setFiles("x.jpg","o.jpg" );
 board.repaint();
 Scanner txtScan = new Scanner(System.in);
 //actuall code stuff
 while(!gameEnd){
 	if(lastPlay%2 == 0){
-		//player 2 is playing
-		xo = '0';
+	//player 1 is playing
+				xo = 'o';
 		    System.out.println("Player 2 is playing");
-		    		                 		    System.out.println("Where do u want to put it");
-		                 String where = txtScan.nextLine();
-		                placeBlock(where);
-updateGameBoard();
-
-
-	} else {
-		//player 1 is playing
-				xo = 'x';
-		    System.out.println("Player 1 is playing");
 		    		                 		    System.out.println("Where do u want to put it");
              String where = txtScan.nextLine();
             placeBlock(where);
 
 updateGameBoard();
 
+		printGameArr();
+
+
+	} else {
+		//player 1 is playing
+				xo = 'x';
+		    System.out.println("Player 1 is playing");
+		    System.out.println("Where do u want to put it");
+             String where = txtScan.nextLine();
+            placeBlock(where);
+
+updateGameBoard();
+
+		printGameArr();
 
 	}
 	lastPlay++;
 }
 
 	}
+
 	public static void updateGameBoard(){
 		board.setBoard(arr);
 		board.repaint();
 	}
+		public static void checkWinner(){
+			char store = 'g';
+				for(int i = 0; i < arr.length; i++){
+					for(int ii = 0; ii < arr[i].length; ii++){
+						if(ii == 1){
+							store = arr[i][ii];
+						}else{
+						if(store != arr[i][ii]){
+                                 break ;
+						}else{
+							//winner(arr[i][ii]);
+						}
+
+						}
+				}
+			}
+
+							for(int i = 0; i < arr.length; i++){
+								for(int j = 0; j < 3; j++){
+									if(j == 1){
+										store = arr[i][j];
+									}else{
+									if(store != arr[i][j]){
+			                                 break ;
+									}else{
+										//winner(arr[i][ii]);
+									}
+
+									}
+							}
+			}
+	}
+
+		public static void printGameArr(){
+			for(int i = 0; i < arr.length; i++){
+				for(int ii = 0; ii < arr[i].length; ii++){
+								System.out.print(arr[i][ii]);
+			}
+											System.out.println(" ");
+			}
+	}
 		public static void placeBlock(String where){
 			 switch(where) {
 						   case "1":
-						     block1 = xo;
+						     arr[0][0] = xo;
 						     break;
 						   case "2":
-						     block2 = xo;
+						     arr[0][1] = xo;
 						     break;
 						        case "3":
-							 		     block3 = xo;
+							 		      arr[0][2] = xo;
 						     break;
 						        case "4":
-							 		     block4 = xo;
+							 		     arr[1][0] = xo;
 						     break;
 						        case "5":
-							 		     block5 = xo;
+							 		     arr[1][1] = xo;
 						     break;
 						        case "6":
-							 		     block6 = xo;
+							 		     arr[1][2] = xo;
 						     break;
 						        case "7":
-							 		     block7 = xo;
+							 		     arr[2][0] = xo;
 						     break;
 						        case "8":
-							 		     block8 = xo;
+							 		     arr[2][1] = xo;
 						     break;
 						        case "9":
-							 		     block9 = xo;
+							 		      arr[2][2]  = xo;
 					     break;
 
 		}
